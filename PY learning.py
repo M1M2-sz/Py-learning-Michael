06c582e60a56184eddfd8f826ae1541dac242b21
M1M2-sz <michael1992avia@gmail.com>
2022-06-08 15:44:20 +0300

    Update PY learning.py

--- PY learning.py
+++ PY learning.py
@@ -3,22 +3,23 @@
 ##learning py
 
 ##Lists
+##String type list
 mylist = ["apple","banana","cherry"]
 print(mylist)
-## index
+## index print
 mylist = ["apple","banana","cherry"]
 print(mylist[0])
 print(mylist[2])
-## index
+## index print
 mylist = ["apple","banana","cherry","orange","kiwi","mango"]
 print(mylist[1:3])
-## index
+## index print
 mylist = ["apple","banana","cherry","orange","kiwi","mango"]
 print(mylist[:3])
-## index
+## index print
 mylist = ["apple","banana","cherry","orange","kiwi","mango"]
 print(mylist[3:])
-## -index
+## -index print
 mylist = ["apple","banana","cherry","orange","kiwi","mango"]
 print(mylist[-5:-3])
 ## search string
@@ -40,12 +41,16 @@ list1 = ["string1",1,True,33,"bogdan"]
 ## list constructor list
 mylist = list(("string1","string2","string3"))
 print(mylist)
-##
+## index print
 mylist = ["apple","banana","cherry"]
 print(mylist[0])
 print(mylist[-2])
-##
+## search string in list 
 mylist = ["apple","banana","cherry","orange","kiwi","mango"]
 if "apple" in mylist:
     print("Yes,this 'apple' in list")
-
+##Edit list
+mylist = ["apple","banana","cherry","orange","kiwi","mango"]
+mylist[1] = "banana1"
+print(mylist) 
+##
