cf9f47e2422e8936916549642b9b85dfa5c1a5ac
M1M2-sz <michael1992avia@gmail.com>
2022-07-05 12:46:29 +0300

    Create test101.py

---
+++ test101.py
@@ -0,0 +1,41 @@
+import numpy as np
+from PIL import Image, ImageDraw
+import sys
+
+sys.setrecursionlimit(150000)
+
+def fillarea(baba,x,y):
+  if (x<0) or (y<0) or (x>=baba.shape[1]) or (y>=baba.shape[0]):
+    return 0
+  valuee=baba[y,x]
+  if valuee[1]==255:# search green 
+    return 0
+  baba[y,x]=[0,255,0];#green fill 
+  area=1 
+  for y1 in range(-1,2):#range 3 step
+    for x1 in range(-1,2):#range 3 step
+      area=area+fillarea(baba,x+x1,y+y1)#(recursion) - function restart with a new param
+  return area
+
+
+image_1 = Image.open('/Users/M1/Downloads/pix1.jpg')
+baba = np.array(image_1)
+
+
+#Method3
+count=0
+totalarea=0
+for y in range(image_1.height):
+ for x in range(image_1.width):
+  valuee=baba[y,x]
+  if valuee[0]!=0 and valuee[1]!=255 and valuee[2]!=0:
+   count+=1
+   area=fillarea(baba,x,y)
+   print(' i have found ',count,' figure with area = ',area)
+   totalarea+=area
+print('total area is ',totalarea)
+
+img = Image.fromarray(baba)
+img.show()
+
+
