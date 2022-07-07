062b482f08a5459d8872c4869bb04b39c1c977cc
M1M2-sz <michael1992avia@gmail.com>
2022-07-07 12:35:41 +0300

    add

---
+++ natural_numbers.py
@@ -0,0 +1,23 @@
+
+
+def NatNum(x):
+ sum=1
+ for b in range(1,x+1):
+   sum=sum*b
+ return sum;
+
+ print(NatNum(1,250))
+
+
+def NatNum(x,y):
+    if x==1:
+        print('')
+        x += 1
+    for a in range(x, y+1):
+        for b in range(2, a+1):
+            if a % b == 0:          
+                break              
+        if b == a:                  
+            print(a,end=" ")
+    print('')
+NatNum(1,500)
